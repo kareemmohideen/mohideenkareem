a03b7b509acd1f2b3ebc57fad5246806e477a489
Kareem <Kareem@DESKTOP-M21ODEG>
2022-01-27 18:53:26 +0530

    second commmit

---
+++ sirius/src/kareem/primitivedata.java
@@ -0,0 +1,23 @@
+package kareem;
+
+public class primitivedata {
+	int a;
+	byte b;
+	long l;
+	short s;
+	boolean bo;
+	float f;
+	double d;
+public static void main(String[] args) {
+	primitivedata prim = new primitivedata();
+	System.out.println(prim.a);
+	System.out.println(prim.b);
+	System.out.println(prim.l);
+	System.out.println(prim.s);
+	System.out.println(prim.bo);
+	System.out.println(prim.f);
+	System.out.println(prim.d);
+	
+}
+
+}
