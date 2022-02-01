2a3e1672c46a1800a81ed01a8b60fcee095a85f2
Kareem <Kareem@DESKTOP-M21ODEG>
2022-02-01 18:15:54 +0530

    ascending array

--- sirius/src/kareem/arrayascend.java
+++ sirius/src/kareem/arrayascend.java
@@ -5,7 +5,7 @@ public class arrayascend {
 		int[] kareemarray=new int[] {88,34,23,45,67,77};
 		for(int i=0;i<kareemarray.length;i++) {
 			for(int j=i;j<kareemarray.length;j++) {
-				if(kareemarray[i]<kareemarray[j]) {
+				if(kareemarray[i]>kareemarray[j]) {
 					int temp=kareemarray[i];
 					kareemarray[i]=kareemarray[j];
 					kareemarray[j]=temp;
